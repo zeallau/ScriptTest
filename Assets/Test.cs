db5f3dcaca463e3d40239ddcdfe43453604a1b7d
zeallau <lauzeal@gmail.com>
2018-01-29 21:36:39 +0900

    ScriptTest first commit I would make it as first version, only finished the first part of homework.

---
+++ Assets/Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        int[] points = { 3, 6, 9, 12, 15 };
+        for (int i = 0; i < points.Length; i++)
+        {
+            Debug.Log(points[i]);
+        }
+
+        Debug.Log(points.Length);
+
+        
+        for (int i = points.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(points[i]);
+        }
+        
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
