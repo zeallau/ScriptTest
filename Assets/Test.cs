d9b63919d4a1f1a903ec7ea9ee7e56ee6c66256d
zeallau <lauzeal@gmail.com>
2018-01-29 22:16:40 +0900

    kadai 4, version 2 how to switch between version 1 and version 2?

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,11 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+public class Boss
+{
+    private int mp = 53;
+    private int spell = 5;
+
+    //Magic function
+    public void Magic()
+    {
+        //decrease mp when spell
+        this.mp -= spell;
+
+        
+     if(mp >= 5)
+     {
+         Debug.Log("魔法攻撃をした。残りMPは" + mp);
+     }
+     else
+     {
+         Debug.Log("MPが足りないため魔法が使えない。");
+     }
+     
+
+    }
+
+
+}
+
+
 public class Test : MonoBehaviour
 {
 
+
+
     void Start()
     {
+        Boss boss = new Boss();
+
+        boss.Magic();
+        boss.Magic();
+        boss.Magic();
+        boss.Magic();
+        boss.Magic();
+        boss.Magic();
+        boss.Magic();
+        boss.Magic();
+        boss.Magic();
+        boss.Magic();
+
+        //そんな馬鹿に10回貼り付けじゃないでしょう？
+
+
+        /*
         int[] points = { 3, 6, 9, 12, 15 };
         for (int i = 0; i < points.Length; i++)
         {
@@ -21,7 +68,7 @@ public class Test : MonoBehaviour
         {
             Debug.Log(points[i]);
         }
-        
+        */
 
     }
 
